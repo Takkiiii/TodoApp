9756b3b9d805575bc543c0764ab34161b23cad2c
ShoTakizawa <shotakizawa0204@gmail.com>
2019-03-24 21:32:45 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,138 +1,149 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
-const kContacts = const <Contact>[
-    const Contact(
-      fullName: 'Romain Hoogmoed',
-      email:'romain.hoogmoed@example.com'
-    ),
-    const Contact(
-      fullName: 'Emilie Olsen',
-      email:'emilie.olsen@example.com'
-    )
-];
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-      ),
-      home: new ContactsPage()
-    );
-  }
+  MyAppState createState() => new MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+class MyAppState extends State<MyApp> {
+  List<String> rows = new List<String>()
+    ..add('Row 1')
+    ..add('Row 2')
+    ..add('Row 3')
+    ..add('Row 4');
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    final title = 'Sortable ListView';
+
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
         appBar: new AppBar(
-          title: new Text("Material App"),
+          title: new Text(title),
         ),
-        body: ListView(
-            children: List.generate(10, (index) {
-          return InkWell(
-            onTap: () {
-            },
-            child: Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset("assets/screen_01.png"),
-                  Container(
-                      margin: EdgeInsets.all(10.0),
-                      child: ListTile(
-                        title: Text("screen_01.png"),
-                        leading: Icon(Icons.person),
-                        subtitle: Text("サブタイトル"),
-                      )),
-                ],
+        body: new SortableListView(
+          items: rows,
+          itemBuilder: (_, int index) => new Card(
+                child: new ListTile(
+                    leading: new Icon(Icons.photo),
+                    title: new Text(rows[index])),
               ),
-            ),
-          );
-        })));
-  }
-}
-
-class Contact {
-  final String fullName;
-  final String email;
-
-  const Contact({this.fullName, this.email});
-}
-
-class _ContactListItem extends StatelessWidget {
-  final Contact _contact;
-  bool isSelected = false;
-
-  _ContactListItem(this._contact);
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        // navigate page
-      },
-      child: Card(
-        child: Column(
-          children: <Widget>[
-            Container(
-            margin: EdgeInsets.all(10.0),
-            child: ListTile(
-              title: Text(_contact.fullName),
-              leading: Image.asset("assets/screen_01.png", height: 50),
-              subtitle: Text(_contact.email),
-            )),
-          ],
         ),
-      )
+      ),
     );
   }
 }
 
-class ContactList extends StatelessWidget {
+class SortableListView extends StatefulWidget {
+  final List items;
+  final IndexedWidgetBuilder itemBuilder;
 
-  final List<Contact> _contacts;
-
-  ContactList(this._contacts);
+  SortableListView({this.items, this.itemBuilder})
+      : assert(items != null),
+        assert(itemBuilder != null);
 
   @override
-  Widget build(BuildContext context) {
-    return ListView(
-          padding: EdgeInsets.symmetric(vertical: 8.0),
-          children: _buildContactList()
-        );
-  }
-
-  List<_ContactListItem> _buildContactList() {
-    return _contacts.map((contact) => _ContactListItem(contact))
-                    .toList();
-  }
-
+  State createState() => new SortableListViewState();
 }
 
-class ContactsPage extends StatelessWidget {
-
+class SortableListViewState extends State<SortableListView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: new Text("Contacts"),
-        ),
-        body: ContactList(kContacts)
-      );
+    return new LayoutBuilder(
+      builder: (context, constraint) {
+        return new ListView.builder(
+          itemCount: widget.items.length + 1,
+          addRepaintBoundaries: true,
+          itemBuilder: (context, index) {
+            return new LongPressDraggable<int>(
+              data: index,
+              child: new DragTarget<int>(
+                onAccept: (int data) {
+                  _handleAccept(data, index);
+                },
+                builder: (BuildContext context, List<int> data,
+                    List<dynamic> rejects) {
+                  List<Widget> children = [];
+
+                  // If the dragged item is on top of this item, the we draw
+                  // a half-visible item to indicate that dropping the dragged
+                  // item will add it in this position.
+                  if (data.isNotEmpty) {
+                    children.add(
+                      new Container(
+                        decoration: new BoxDecoration(
+                          border: new Border.all(
+                              color: Colors.grey[600], width: 2.0),
+                        ),
+                        child: new Opacity(
+                          opacity: 0.5,
+                          child: _getListItem(context, data[0]),
+                        ),
+                      ),
+                    );
+                  }
+                  children.add(_getListItem(context, index));
+
+                  return new Column(
+                    children: children,
+                  );
+                },
+              ),
+              onDragStarted: () {
+                Scaffold.of(context).showSnackBar(
+                      new SnackBar(
+                          content: new Text("Drag the row to change places")),
+                    );
+              },
+              feedback: new Opacity(
+                opacity: 0.75,
+                child: new SizedBox(
+                  width: constraint.maxWidth,
+                  child: _getListItem(context, index, true),
+                ),
+              ),
+              childWhenDragging: new Container(),
+            );
+          },
+        );
+      },
+    );
   }
 
+  void _handleAccept(int data, int index) {
+    setState(() {
+      // Decrement index so that after removing we'll still insert the item
+      // in the correct position.
+      if (index > data) {
+        index--;
+      }
+      dynamic imageToMove = widget.items[data];
+      widget.items.removeAt(data);
+      widget.items.insert(index, imageToMove);
+    });
+  }
+
+  Widget _getListItem(BuildContext context, int index, [bool dragged = false]) {
+    // A little hack: our ListView has an extra invisible trailing item to
+    // allow moving the dragged item to the last position.
+    if (index == widget.items.length) {
+      // This invisible item uses the previous item to determine its size. If
+      // the list is empty, though, there's no dragging really.
+      if (widget.items.isEmpty) {
+        return new Container();
+      }
+      return new Opacity(
+        opacity: 0.0,
+        child: _getListItem(context, index - 1),
+      );
+    }
+
+    return new Material(
+      elevation: dragged ? 20.0 : 0.0,
+      child: widget.itemBuilder(context, index),
+    );
+  }
 }
